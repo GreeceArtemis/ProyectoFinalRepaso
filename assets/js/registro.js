4b9cfcce9321a23bc34c663fdc0bd6fcaf94f222
GreeceArtemis <greece.rapu@gmail.com>
2017-03-29 13:50:37 -0500

    v3

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,47 +1,71 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta http-equiv="X-UA-Compatible" content="ie=edge">
-          <title>Registro</title>
-          <link rel="stylesheet" href="assets/css/main.css">
-    </head>
-    <body>
-        <div id="ventanaRegistro">
-        <h1>Registro</h1>
-            <form id="registroCoder">
-              <label>Nombre:</label>
-              <br>
-              <input type="text" id="nombre" class="datoRegistro" placeholder="Ingresa tu Nombre" >
-              <br>
-              <span class="indicacionLetras"></span>
-              <br>
-              <label>Apellido </label>
-              <br>
-              <input type="text" id="apellido" class="datoRegistro" placeholder="Ingresa tu Apellido"/>
-              <br>
-              <span class="indicacionLetras"></span>
-              <br>
-              <label>Correo:</label>
-              <br>
-              <input type="text" id="correo" class="datoRegistro" placeholder="Correo" >
-              <br>
-              <span class="indicacionCorreo"></span>
-              <br>
-              <label>Contraseña</label>
-              <br>
-              <input type="text" id="contraseña" class="datoRegistro" placeholder="Password"/>
-              <br>
-              <span class="indicacionMax"></span>
-              <br>
-              <button type="submit" id="guardarRegistro">Login</input></button>
-              <br>
-              <label id="mensajeError"></label>
-            </form>
-        </div>
-        <script type="text/javascript" src="assets/js/registro.js"></script>
-    </body>
-</html>
 
+  window.addEventListener('load',function(e) {
+
+    var baseDatos=[];
+    var nombre=document.getElementById("nombre");
+    var apellido=document.getElementById("apellido");
+    var correo=document.getElementById("correo");
+    var contraseña=document.getElementById("contraseña");
+
+    function alldatos(nombre,apellido,correo,contraseña){
+          this.nombre=nombre;
+          this.apellido=apellido;
+          this.correo=correo;
+          this.contraseña=contraseña;
+    };
+
+//VALIDACION DE LETRAS
+    var validarLetras=function(e){
+        var codigoTecla=e.keyCode;
+        if((codigoTecla>=97 && codigoTecla<=122)||(codigoTecla>=65 && codigoTecla<=90)||codigoTecla==39 || codigoTecla==32){
+              this.nextElementSibling.nextElementSibling.innerText = "";
+              return true;
+            }
+        else
+          {
+              this.nextElementSibling.nextElementSibling.innerText = "SOLO LETRAS";
+              return false;
+          }
+    }
+  //
+    nombre.onkeypress=validarLetras;
+    apellido.onkeypress=validarLetras;
+    console.log(nombre.value);
+  //
+    var datosRegistro=document.getElementsByClassName("datoRegistro");
+    console.log(datosRegistro);
+    var validacionDato=function(){
+          if(this.value.trim().length==0){
+                this.value="";
+                this.nextElementSibling.nextElementSibling.innerText="*Este campo es obligatorio";
+            }
+        else {
+                this.nextElementSibling.nextElementSibling.innerText="";
+            }
+       }
+    for(var i=0;i<datosRegistro.length;i++){
+        datosRegistro[i].onblur=validacionDato;
+    }
+
+    guardarRegistro.addEventListener('click',function(e) {
+      e.preventDefault();
+      //VALIDAR TODOS LOS CAMPOS
+          if (nombre.value == "" || apellido.value == "" || correo.value == "" || contraseña.value == ""){
+              document.getElementById("mensajeError").innerHTML="OBLIGATORIO LLENAR EL FORMULARIO"  ;
+          }
+          else{
+              document.getElementById("mensajeError").innerHTML="";
+      //ALMACENO TODOS MIS CODERS
+              var coder=new alldatos (nombre.value,apellido.value,correo.value,contraseña.value);
+              baseDatos.push(coder);
+
+              localStorage.setItem("nuevoCoder",JSON.stringify(coder));
+              localStorage.setItem("baseDatosGeneral",JSON.stringify(baseDatos));
+      //MUESTRO TODOS MIS CODERS
+              console.log(baseDatos);
+              document.getElementById("registroCoder").reset();
+          }
+          window.location="mostrar.html";
+      });
+});
 
