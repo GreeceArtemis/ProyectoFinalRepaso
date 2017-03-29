116cab223f441ed5ff51fc4c6b4e76ba98767ff3
GreeceArtemis <greece.rapu@gmail.com>
2017-03-29 11:29:35 -0500

    bienvenida

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+window.addEventListener("load",function () {
+  document.getElementById("registro").addEventListener("click",function(){
+    window.location="registro.html";
+  })
+  document.getElementById("iniciarSesion").addEventListener("click",function(){
+    window.location="iniciarsesion.html";
+  })
+});
